01e6befe23e0cc7220a8139d11562b8230d63ddf
siv24M <118889892+siv24M@users.noreply.github.com>
2022-11-24 04:55:32 +0000

    Solution for FIZZBUZZ

--- 01_03b/1-3-fizzbuzz.php
+++ 01_03b/1-3-fizzbuzz.php
@@ -1,1 +1,27 @@
 <?php
+
+function fizzbuzz($i){
+ 
+  if (($i%3 == 0) && ($i%5 == 0)){
+   return "FIZZBUZZ";
+  }
+  elseif (($i%3 == 0) && ($i%5 != 0)){
+    return "FIZZ";
+  }
+  elseif (($i%5 == 0 )&&($i%3 !=0)){
+   return "BUZZ";
+  }
+  else{
+   return $i;
+  }
+}
+
+$n = 1;
+while ($n <= 100) {
+echo fizzbuzz($n)."<br>";
+$n++;
+}
+
+
+
+
